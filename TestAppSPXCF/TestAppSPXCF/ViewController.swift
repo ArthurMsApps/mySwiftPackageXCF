9721b2f9267f5007eae1fbd84c4cf22d50467e22
Arthur <arthur@msapps.mobi>
2022-04-20 13:31:05 +0300

    added test app

---
+++ TestAppSPXCF/TestAppSPXCF/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestAppSPXCF
+//
+//  Created by Arthur on 20/04/2022.
+//
+
+import UIKit
+import mySwiftPackageXCF
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let s = mySwiftPackageXCF()
+        s.hiArthur()
+    }
+
+
+}
+
